7d43680d600d2fc3e239605d46e0ee8a79f9fab5
Rupom-Raz <rupomk70@gmail.com>
2022-07-05 15:49:33 +0600

    step fixed

--- JS/main.js
+++ JS/main.js
@@ -9,36 +9,35 @@ var Next3 = document.getElementById("next3");
 var Back1 = document.getElementById("back1");
 var Back2 = document.getElementById("back2");
 var Back3 = document.getElementById("back3");
-var progress = document.getElementById('progress')
+var progress = document.getElementById("progress");
 
-Next1.onclick = function() {
-    Form1.style.left = "-450px"
-    Form2.style.left = "40px"
-    progress.style.width = "180px"
-
-}
-Back1.onclick = function() {
-    Form1.style.left = "40px"
-    Form2.style.left = "450px"
-    progress.style.width = "90px"
-}
-Next2.onclick = function() {
-    Form2.style.left = "-450px"
-    Form3.style.left = "40px"
-    progress.style.width = "270px"
-}
-Back2.onclick = function() {
-    Form2.style.left = "40px"
-    Form3.style.left = "450px"
-    progress.style.width = "180px"
-}
-Next3.onclick = function() {
-    Form3.style.left = "-450px"
-    Form4.style.left = "40px"
-    progress.style.width = "360px"
-}
-Back3.onclick = function() {
-    Form3.style.left = "40px"
-    Form4.style.left = "450px"
-    progress.style.width = "240px"
-}
+Next1.onclick = function () {
+    Form1.style.left = "-1200px";
+    Form2.style.left = "30px";
+    progress.style.width = "50%";
+};
+Back1.onclick = function () {
+    Form1.style.left = "30px";
+    Form2.style.left = "1200px";
+    progress.style.width = "25%";
+};
+Next2.onclick = function () {
+    Form2.style.left = "-450px";
+    Form3.style.left = "40px";
+    progress.style.width = "75%";
+};
+Back2.onclick = function () {
+    Form2.style.left = "40px";
+    Form3.style.left = "450px";
+    progress.style.width = "50%";
+};
+Next3.onclick = function () {
+    Form3.style.left = "-450px";
+    Form4.style.left = "40px";
+    progress.style.width = "100%";
+};
+Back3.onclick = function () {
+    Form3.style.left = "40px";
+    Form4.style.left = "450px";
+    progress.style.width = "75%";
+};
