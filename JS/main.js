471fa4f41d468e6f4f6126fd677715b40131039d
Rupom-Raz <rupomk70@gmail.com>
2022-07-06 13:35:27 +0600

    international checked

--- JS/main.js
+++ JS/main.js
@@ -16,13 +16,9 @@ var step2 = document.getElementById("step2");
 var step3 = document.getElementById("step3");
 var step4 = document.getElementById("step4");
 
-step1.onclick = function () {
-   
-};
+step1.onclick = function () {};
 
-step2.onclick = function() {
-
-}
+step2.onclick = function () {};
 
 Next1.onclick = function () {
     Form1.style.left = "-1200px";
@@ -82,5 +78,50 @@ function FillAddressInput() {
         par_District.value = "";
         par_City.value = "";
         par_Postal.value = "";
+    }
+}
+
+// International Student check
+var selectField = document.getElementById("selectField");
+var bloodGroup = document.getElementById("bloodGroup");
+var photo = document.getElementById("photo");
+var hideBloodGr = document.getElementById("hideBloodGr");
+var birth_cert = document.getElementById("birth_cert");
+var medicalCert = document.getElementById("medicalCert");
+var tribal = document.getElementById("tribal");
+var passNoSpan = document.getElementById("passNoSpan");
+var hidePhoto = document.getElementById("hidePhoto");
+var policeClearance = document.getElementById("policeClearance");
+var passPhoto = document.getElementById("passPhoto");
+var nation_id = document.getElementById("nation_id");
+var pass_expire = document.getElementById("pass_expire");
+var local_hidden = document.getElementById("local_hidden");
+
+
+function interOnchange() {
+    if (selectField.value === "International Student") {
+        bloodGroup.style.display = "block";
+        photo.style.display = "block";
+        pass_expire.style.display = "block";
+        policeClearance.style.display = "block";
+        medicalCert.style.display = "block";
+        passPhoto.style.display = "block";
+        birth_cert.style.display = "none";
+        hideBloodGr.style.display = "none";
+        tribal.style.display = "none";
+        hidePhoto.style.display = "none";
+        nation_id.style.display = "none";
+        passNoSpan.style.visibility = "visible";
+    } else if (selectField.value === "Local Student(Bangladeshi)") {
+        bloodGroup.style.display = "none";
+        photo.style.display = "none";
+        passPhoto.style.display = "none";
+        policeClearance.style.display = "none";
+        medicalCert.style.display = "none";
+        pass_expire.style.display = "none";
+        birth_cert.style.display = "block";
+        nation_id.style.display = "block";
+        hidePhoto.style.display = "block";
+        tribal.style.display = "block";
     }
 }
